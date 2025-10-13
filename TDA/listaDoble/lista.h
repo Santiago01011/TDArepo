8ff4ad6152ddcb5ef40a3307093c17801969f97d
Santiago <66748228+Santiago01011@users.noreply.github.com>
2025-10-13 20:45:06 -0300

    Add files via upload
    listaDobleDin

---
+++ TDA/listaDoble/lista.h
@@ -0,0 +1,26 @@
+#ifndef LISTA_H_INCLUDED
+#define LISTA_H_INCLUDED
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+typedef struct sNodo{
+    void *dato;
+    unsigned tamDato;
+    struct sNodo *sig;
+    struct sNodo *ant;
+}tNodo;
+
+typedef tNodo *tLista;
+
+void crearLista(tLista*lista);
+int insertarOrdenado(tLista*lista, const void*dato, unsigned tamDato, int(*comparar)(const void*, const void*));
+void maplistaIzq(tLista*lista, void (*accion)(const void*));
+void maplistaDer(tLista*lista, void (*accion)(const void*));
+int cmpint(const void *x, const void *y);
+void imprimirInt(const void * dato);
+void destruirLista(tLista*lista);
+
+
+#endif // LISTA_H_INCLUDED
